d88edbac42e07d4442637d90522acd8335d0169c
Filipe Natanael <filipenatanael@live.com>
2019-01-06 23:12:34 -0200

    feat: create database seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,19 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    factory('App\Models\User', 10)->create();
+    // $this->call(UsersTableSeeder::class);
+    // DB::table('users')->insert([
+    //   'name' => str_random(10),
+    //   'email' => str_random(10).'@gmail.com',
+    //   'password' => bcrypt('secret'),
+    // ]);
+  }
 }
