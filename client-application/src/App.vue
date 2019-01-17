ab9aeeb75ecd318406a074dcb89666a40eb4d192
Filipe Natanael <filipenatanael@live.com>
2019-01-17 19:20:25 -0200

    feat: adjustment on client application

--- client-application/src/App.vue
+++ client-application/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <ApolloExample msg="Welcome to Your Vue.js App"/>
+    <hello-world />
   </div>
 </template>
 
 <script>
-import ApolloExample from './components/ApolloExample.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    ApolloExample
+    HelloWorld
   }
 }
 </script>
